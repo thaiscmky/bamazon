c648058ece8ec0232764acf5eef16979d9f020a1
Thais Cailet <thais.cailet@gmail.com>
2018-04-21 01:16:31 -0500

    Closes #6 Create script to install sample data

--- scripts/setup/database.js
+++ scripts/setup/database.js
@@ -1,8 +1,21 @@
+require("dotenv").config();
 var mysql = require("mysql");
+var csvparse = require("csv-parse");
+var fs = require("fs");
+var connection =  mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
+});
+connection.connect(function(err) {
+    if (err) {
+        console.error('Error connecting: ');
+        throw err.stack;
+    }
+});
+
 var Database = function(){
-    this.host = process.env.DB_HOST;
-    this.user = process.env.DB_USER;
-    this.password = process.env.DB_PASSWORD;
     this.database = process.env.DB_NAME;
     this.installSchema = function (tables) {
         let self = this;
@@ -19,18 +32,52 @@ var Database = function(){
      */
     this.createTable = function(tablename, args) {
         let columns = Object.keys(args);
-        let sql = `CREATE TABLE ${tablename}(`;
+        let sql = `CREATE TABLE IF NOT EXISTS ${this.database}.${tablename}(`;
         columns.forEach(function(column, index){
-           if(index < args.length - 1)
+           if(index < columns.length - 1)
                sql += `${column} ${args[column].join(' ')},`;
            else
                sql += `${column} ${args[column].join(' ')}`;
         });
         sql += `);`;
+        connection.query(sql, function (error) {
+            if (error) throw error;
+            console.log(`Table ${tablename} successfully created.`);
+        });
 
     };
     this.createDatabase = function() {
-        let sql = `CREATE DATABASE IF NOT EXISTS ${this.database};`;
+        let self = this;
+        let sql = `CREATE DATABASE IF NOT EXISTS ${self.database};`;
+        connection.query(sql, function (error) {
+            if (error) throw error;
+            console.log(`Database ${self.database} successfully created.`);
+            connection.database = self.database;
+        });
+    };
+    this.import = function(filepath, table){
+        let self = this;
+        fs.readFile(filepath, { encoding: 'utf-8'}, function(err, csvdata) {
+            if (err) {
+                console.log('Something went wrong while reading your file: '+filepath);
+                throw err;
+            }
+
+            csvparse(csvdata, { columns: true, delimiter: ',' }, function(err, data) {
+                if (err)
+                    throw err;
+                data.forEach(function(rowObj){
+                    let fields = Object.keys(rowObj);
+                    let values = Object.values(rowObj).map(function(value){ return Number(value) ? value : `"${value}"`; });
+                    let sql = `INSERT INTO ${self.database}.${table}(${fields.join(', ')}) VALUES(${values.join(',')});`;
+                    connection.query(sql, function (error) {
+                        if (error) throw error;
+                    });
+                });
+            }).on('finish', function(){
+                console.log(`Import of ${filepath} complete.`);
+            });
+        });
     }
 };
 
--- scripts/setup/install.js
+++ scripts/setup/install.js
@@ -1,22 +1,29 @@
 var Database = require(__dirname + '/database.js');
 
 var db = new Database();
-db.createTable(
-    'products',
+console.log('Creating database schema...');
+db.installSchema(
+    [{
+        name: 'products',
+        columns: {
+            item_id: ['INT', 'AUTO_INCREMENT', 'PRIMARY KEY'],
+            product_name: ['VARCHAR(100)', 'NOT NULL'],
+            department_name: ['VARCHAR(100)'],
+            price: ['DECIMAL(10,2)', 'NOT NULL'],
+            stock_quantity: ['INT', 'DEFAULT 1']
+        }
+    },
     {
-        item_id: ['INT', 'AUTO_INCREMENT', 'PRIMARY KEY'],
-        product_name: ['VARCHAR(100)', 'NOT NULL'],
-        department_name: ['VARCHAR(100)'],
-        price: ['DECIMAL(10,2)', 'NOT NULL'],
-        stock_quantity: ['INT', 'DEFAULT 1']
-    }
+        name: 'departments',
+        columns: {
+            department_id: ['INT', 'AUTO_INCREMENT', 'PRIMARY KEY'],
+            department_name: ['VARCHAR(100)', 'NOT NULL'],
+            over_head_costs: ['DECIMAL(10,2)', 'DEFAULT 0'],
+            product_sales: ['DECIMAL(10,2)', 'DEFAULT 0']
+        }
+    }]
 );
-db.createTable(
-    'departments',
-    {
-        department_id: ['INT', 'AUTO_INCREMENT', 'PRIMARY KEY'],
-        department_name: ['VARCHAR(100)', 'NOT NULL'],
-        over_head_costs: ['DECIMAL(10,2)', 'DEFAULT 0'],
-        product_sales: ['DECIMAL(10,2)', 'DEFAULT 0']
-    }
-);
+
+console.log('Adding sample data...');
+db.import(__dirname + '/data/initialprods.csv', 'products');
+db.import(__dirname + '/data/initialdepartments.csv', 'departments');
