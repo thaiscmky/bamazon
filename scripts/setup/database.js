83b7fd86ee7f9a432bcec7ffde45e5b4f3e80505
Thais Cailet <thais.cailet@gmail.com>
2018-04-20 22:46:58 -0500

    Closes #5 Add logic for installing db schema

--- scripts/setup/database.js
+++ scripts/setup/database.js
@@ -4,14 +4,35 @@ var Database = function(){
     this.user = process.env.DB_USER;
     this.password = process.env.DB_PASSWORD;
     this.database = process.env.DB_NAME;
-    this.installSchema = function () {
-        this.createDatabase();
-        this.createTable();
+    this.installSchema = function (tables) {
+        let self = this;
+        self.createDatabase();
+        tables.forEach(function(tableObj){
+            self.createTable(tableObj.name, tableObj.columns) ;
+        });
     };
-    this.createTable = function() {
+    /***
+     * Creates a table with columns based on the object key and property values
+     * @param string tablename
+     * @param object args
+     * @return void
+     */
+    this.createTable = function(tablename, args) {
+        let columns = Object.keys(args);
+        let sql = `CREATE TABLE ${tablename}(`;
+        columns.forEach(function(column, index){
+           if(index < args.length - 1)
+               sql += `${column} ${args[column].join(' ')},`;
+           else
+               sql += `${column} ${args[column].join(' ')}`;
+        });
+        sql += `);`;
 
     };
     this.createDatabase = function() {
-
+        let sql = `CREATE DATABASE IF NOT EXISTS ${this.database};`;
     }
 };
+
+module.exports = Database;
+
--- scripts/setup/install.js
+++ scripts/setup/install.js
@@ -1,3 +1,23 @@
-/**
- * Created by dynom on 4/20/2018.
- */
+var Database = require(__dirname + '/database.js');
+
+var db = new Database();
+db.createTable(
+    'products',
+    {
+        item_id: ['INT', 'AUTO_INCREMENT', 'PRIMARY KEY'],
+        product_name: ['VARCHAR(100)', 'NOT NULL'],
+        department_name: ['VARCHAR(100)'],
+        price: ['DECIMAL(10,2)', 'NOT NULL'],
+        stock_quantity: ['INT', 'DEFAULT 1']
+    }
+);
+db.createTable(
+    'departments',
+    {
+        department_id: ['INT', 'AUTO_INCREMENT', 'PRIMARY KEY'],
+        department_name: ['VARCHAR(100)', 'NOT NULL'],
+        over_head_costs: ['DECIMAL(10,2)', 'DEFAULT 0'],
+        product_sales: ['DECIMAL(10,2)', 'DEFAULT 0'],
+        stock_quantity: ['INT', 'DEFAULT 1']
+    }
+);
