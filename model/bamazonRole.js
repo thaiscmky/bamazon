0185ecd0a09aec710b81cdbd70a934002f567e0a
Thais Cailet <thais.cailet@gmail.com>
2018-04-22 23:46:28 -0500

    Simplifies Role into a single object literal

--- model/bamazonRole.js
+++ model/bamazonRole.js
@@ -2,34 +2,35 @@ require("dotenv").config();
 var mysql = require("mysql");
 var Role = function(){};
 
-Role.prototype.inquirer = require("inquirer");
-Role.prototype.connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: 3306,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
-Role.prototype.role = '';
-Role.prototype.options = [];
-Role.prototype.getOptions = function(){
-  return this.options;
+Role.prototype = {
+    role: null,
+    options: [],
+    inquirer: require("inquirer"),
+    connection: mysql.createConnection({
+        host: process.env.DB_HOST,
+        port: 3306,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
+    }),
+    getOptions: function(){
+        return this.options;
+    },
+    addOption: function(id, args, callback){
+        var promptObj = {
+            name: args.name,
+            type: args.type,
+            message: args.message
+        };
+        if(args.validate) promptObj.validate = args.validate;
+        if(args.choices) promptObj.choices = args.choices;
+        this.options.push( {id: { prompt: promptObj, callback: callback} });
+    },
+    promptUser: function(questions, callback){
+        this.inquirer.prompt(questions)
+            .then(function(answer) {
+                callback.call(this);
+            });
+    }
 };
-Role.prototype.addOption = function(id, args, callback){
-  var promptObj = {
-      name: args.name,
-      type: args.type,
-      message: args.message
-  };
-  if(args.validate) promptObj.validate = args.validate;
-  if(args.choices) promptObj.choices = args.choices;
-  this.options.push( {id: { prompt: promptObj, callback: callback} });
-};
-Role.prototype.promptUser = function(questions, callback){
-    this.inquirer.prompt(questions)
-        .then(function(answer) {
-            callback.call(this);
-        });
-};
-
 module.exports = Role;
